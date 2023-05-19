8f49ceec2749d3049191dd368e1983e1bdec7db3
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-05-19 00:02:49 -0300

    Ajusted HeaderLogo component

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const HeaderLogo: React.FC = () => {
+export const HeaderLogo = () => {
+  const [atribute, setAtribute] = useState('bg-white w-2 text-white');
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      if (atribute == 'bg-white w-2 text-white') setAtribute('bg-black w-2 text-black');
+      else setAtribute('bg-white w-2 text-white');
+    }, 1000);
+  });
+
   return (
     <>
-      <div className='flex items-center font-mono w-1/3 max-sm:w-auto text-white'>
-        <h1 className='text-2xl mx-2 font-bold'>
-          <span className='text-green'>ronald_barbosa@dev</span>:<span className='text-blue'>~</span>$ portfolio
+      <div className='flex font-mono w-1/3 max-sm:w-auto text-white'>
+        <h1 className='flex text-2xl mx-2 font-bold p-3'>
+          <span className='text-green'>ronald_barbosa@dev</span>:<span className='text-blue'>~</span>$ portfolio<span className={atribute}></span>
         </h1>
       </div>
     </>
