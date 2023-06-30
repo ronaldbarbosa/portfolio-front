7fb61ba915c48929fc545753015db7c81f992042
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-06-29 22:04:23 -0300

    Applying responsive mode to ContentGroup component

--- src/components/About/ContentGroup/ContentGroup.tsx
+++ src/components/About/ContentGroup/ContentGroup.tsx
@@ -4,7 +4,7 @@ import Title from "./Title";
 
 export default function ContentGroup() {
   return(
-    <div className="flex flex-col w-2/3 mx-5 ml-10 text-white">
+    <div className="flex flex-col w-2/3 max-lg:w-full mx-5 ml-10 text-white">
       <Role />
       <Title />
       <Description />
--- src/components/About/ContentGroup/Description.tsx
+++ src/components/About/ContentGroup/Description.tsx
@@ -1,11 +1,11 @@
 export default function Description() {
   return(
     <>
-      <p className="w-2/3 mt-5">
+      <p className="w-5/6 mt-5">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eligendi laudantium velit sit quaerat voluptates.
         Voluptas perspiciatis amet in sed sequi, rem nulla asperiores veniam, quia sit blanditiis deleniti maxime!
       </p>
-      <p className="w-2/3 mt-5">
+      <p className="w-5/6 mt-5">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eligendi laudantium velit sit quaerat voluptates.
         Voluptas perspiciatis amet in sed sequi, rem nulla asperiores veniam, quia sit blanditiis deleniti maxime!
       </p>
