9989cefbdc8988eb36da30247321a4a838657480
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-06-30 19:04:07 -0300

    Adjusting responsive mode in HeaderLogo component

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -13,7 +13,7 @@ export const HeaderLogo = () => {
   return (
     <>
       <div className='flex font-mono min-w-fit max-sm:w-screen max-md:w-screen max-sm:justify-center max-md:justify-center text-white'>
-        <h1 className='flex text-3xl max-sm:text-lg max-md:text-2xl max-lg:text-xl font-bold mx-2 max-sm:mx-0 p-3'>
+        <h1 className='flex text-3xl max-sm:text-base max-md:text-2xl max-lg:text-xl font-bold mx-2 max-sm:mx-0 p-3'>
           <span className='text-green'>ronald_barbosa@dev</span>:<span className='text-blue'>~</span>$ portfolio<span className={atribute}></span>
         </h1>
       </div>
