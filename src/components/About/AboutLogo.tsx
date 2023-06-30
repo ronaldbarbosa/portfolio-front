f174fd62d669b1344edd3948574f36885e46c835
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-06-29 21:58:52 -0300

    Applying responsive mode to AboutLogo component

--- src/components/About/AboutLogo.tsx
+++ src/components/About/AboutLogo.tsx
@@ -1,8 +1,7 @@
 export default function AboutLogo() {
   return(
-    <div className="flex w-1/3 justify-end mx-10">
-      <span className="text-9xl p-20 py-32 rounded-full outline text-white bg-gradient-to-r from-blue to-green">&#123; &#47; &#125;</span>
+    <div className="hidden lg:flex w-1/3 justify-end items-center mx-10">
+      <span className="text-9xl p-20 py-32 xl:py-28 xl:text-8xl max-xl:text-7xl max-xl:py-28 rounded-full outline text-white bg-gradient-to-r from-blue to-green">&#123; &#47; &#125;</span>
     </div>
-
   );
 }
