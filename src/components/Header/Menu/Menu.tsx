816fc2cf3e9e7c7dff21873bb591c910dc7a39df
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-07-03 00:11:25 -0300

    Bug fix: access Portfolio page from projects details page

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -11,7 +11,7 @@ export const Menu: React.FC = () => {
     <nav className='flex w-2/3 max-md:w-full max-sm:w-full max-md:justify-center justify-end items-center max-sm:pt-3 max-sm:flex-col'>
       <ul className='flex max-sm:hidden items-center text-grey'>
         <MenuItem label="Home" to="/"/>
-        <MenuItem label="Projects" to="projects"/>
+        <MenuItem label="Projects" to="/projects"/>
         <MenuItem label="Contact" to=""/>
       </ul>
       <button className="hidden max-sm:flex w-full justify-center text-3xl text-green" onClick={ () => setOpen(!open) }>
@@ -22,7 +22,7 @@ export const Menu: React.FC = () => {
         open ? (
           <ul className='flex text-grey flex-col w-full mt-3 items-center'>
             <MenuItem label="Home" to="/"/>
-            <MenuItem label="Projects" to="projects"/>
+            <MenuItem label="Projects" to="/projects"/>
             <MenuItem label="Contact" to=""/>
           </ul>
         ) : null
