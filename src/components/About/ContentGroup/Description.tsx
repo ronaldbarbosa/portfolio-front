73e0acf57606c62b9181df3ceaef0fe385531dc5
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-07-05 22:00:41 -0300

    Update description

--- src/components/About/ContentGroup/Description.tsx
+++ src/components/About/ContentGroup/Description.tsx
@@ -2,12 +2,17 @@ export default function Description() {
   return(
     <>
       <p className="w-5/6 mt-5">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eligendi laudantium velit sit quaerat voluptates.
-        Voluptas perspiciatis amet in sed sequi, rem nulla asperiores veniam, quia sit blanditiis deleniti maxime!
+        I am Ronald Barbosa, <strong>a passionate Fullstack Developer</strong> with a strong dedication to
+        <span className="text-green"> C#</span>, <span className="text-green">Python</span> and <span className="text-green">React.js</span>.
+        With little more than a year of studies, I have honed my skills and knowledge to apply them in some personal and college projects.
       </p>
       <p className="w-5/6 mt-5">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eligendi laudantium velit sit quaerat voluptates.
-        Voluptas perspiciatis amet in sed sequi, rem nulla asperiores veniam, quia sit blanditiis deleniti maxime!
+        Throughout my studies, I have worked on some projects that have allowed me to showcase my creativity and problem-solving abilities.
+        I thrive in collaborative environments and enjoy working closely with clients, colleagues, and stakeholders to achieve shared goals.
+      </p>
+      <p className="w-5/6 mt-5">
+        On this portfolio page, you will find a selection of my work, demonstrating my knowledge in Frontend, Backend and DevOps.
+        Each project represents a milestone in my journey and showcases my passion for Web Development.
       </p>
     </>
   );
