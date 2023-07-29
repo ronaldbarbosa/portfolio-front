120c794ed0e0ac7409387016374b6671e28db98d
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-07-28 21:54:02 -0300

    Initial configuration for i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+import { appWithTranslation } from 'next-i18next';
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default appWithTranslation(App);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,30 @@ import About from "@/components/About/About"
 import Footer from "@/components/Footer/Footer"
 import { Header } from "@/components/Header/Header"
 
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export interface PageProps {
+  locale: string
+}
+
+export async function getStaticProps(props: PageProps) {
+  return {
+    props: {
+      ...(await serverSideTranslations(props.locale, ['index']))
+    }
+  }
+}
+
 export default function Home() {
+  const { t } = useTranslation()
+
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Head>
         <title>Portfolio</title>
       </Head>
+      <h1 className="text-white">{ t('index:dev_title') }</h1>
       <Header />
       <About />
       <Footer />
