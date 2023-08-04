b9754e617e3f3f0b47d18e17704c17276488c8c1
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-08-04 16:11:50 -0300

    Add copy to clipboard icon to contact info

--- src/components/Contact/ContactItems.tsx
+++ src/components/Contact/ContactItems.tsx
@@ -1,25 +1,43 @@
-import { AiOutlineMail, AiOutlinePhone, AiOutlineWhatsApp } from "react-icons/ai";
+import { useCopyToClipboard } from "usehooks-ts";
+import { AiOutlineMail, AiOutlinePhone, AiOutlineWhatsApp, AiOutlineCopy } from "react-icons/ai";
 
 export default function ContactItems() {
+  const [value, copy] = useCopyToClipboard();
+
   return (
     <div className="flex flex-col w-full items-center text-grey text-2xl">
-      <div className="flex flex-col items-center">
+      <address className="flex flex-col items-center">
         <p>Phone</p>
         <div className="flex max-sm:flex-col items-center my-5">
           <AiOutlinePhone className="text-5xl" />
-          <strong className="text-green mx-5 max-sm:mt-1 max-sm:text-xl">+5524999492709</strong>
+          <strong className="text-green mx-5 max-sm:mt-1 max-sm:text-xl">
+            <a href="tel:+5524999492709">+55 24 99949-2709</a>
+          </strong>
+          <button onClick={() => copy('+5524999492709')}>
+            <AiOutlineCopy className="text-xl ml-1 hover:text-green" />
+          </button>
         </div>
         <p>Message</p>
         <div className="flex max-sm:flex-col items-center my-5">
           <AiOutlineWhatsApp className="text-5xl" />
-          <strong className="text-green mx-5 max-sm:mt-1 max-sm:text-xl">+5524999492709</strong>
+          <strong className="text-green mx-5 max-sm:mt-1 max-sm:text-xl">
+            <a href="https://wa.me/5524999492709" target="_blank">+55 24 99949-2709</a>
+          </strong>
+          <button onClick={() => copy('+5524999492709')}>
+            <AiOutlineCopy className="text-xl ml-1 hover:text-green" />
+          </button>
         </div>
         <p>Email</p>
         <div className="flex max-sm:flex-col items-center my-5">
           <AiOutlineMail className="text-5xl" />
-          <strong className="text-green mx-5 max-sm:mt-1 max-sm:text-xl">ronaldverissimo@hotmail.com</strong>
+          <strong className="text-green ml-5 max-sm:mt-1 max-sm:text-xl">
+            <a href="mailto:ronaldverissimo@hotmail.com" target="_blank">ronaldverissimo@hotmail.com</a>
+          </strong>
+          <button onClick={() => copy('ronaldverissimo@hotmail.com')}>
+            <AiOutlineCopy className="text-xl ml-1 hover:text-green" />
+          </button>
         </div>
-      </div>
+      </address>
     </div>
   );
 }
